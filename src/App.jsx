baa54b6511bcdc014b1515ec2a527ee5612b16a6
raushanbyteiq-new <raushanbyteiq@gmail.com>
2025-12-25 09:45:15 -0800

    app.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './HomePage.jsx'
-import WebRTC from './WebRTC.jsx'
+// import WebRTC from './WebRTC.jsx'
 import WebRTCWithTranslation1 from './WebRTCWithBuffer.jsx'
-import AIDashboard from './AiDashboard.jsx'
+// import AIDashboard from './AiDashboard.jsx'
 
 function App() {
   return (
